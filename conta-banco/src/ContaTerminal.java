dc0d7085dd5255d4c15b61a9d6db7847b0867d9f
fabiosimplis <fabio.simplis@gmail.com>
2023-12-30 10:16:20 -0300

    Programa Final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner leituraTerminal = new Scanner(System.in);
+        
+        System.out.println("Por favor digite o numero da agência: ");
+        int agencia = leituraTerminal.nextInt();
+
+        System.out.println("Por favor digite o numero da conta: ");
+        String conta = leituraTerminal.next();
+
+        System.out.println("Por favor digite o seu nome: ");
+        String nome = leituraTerminal.next();
+        leituraTerminal.nextLine();
+
+        System.out.println("Por favor digite o seu saldo: ");
+        float saldo = leituraTerminal.nextFloat();
+
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %s e seu saldo R$%.2f já está disponível para saque.\n", nome, agencia, conta, saldo);
     }
 }
